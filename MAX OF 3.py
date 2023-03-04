05faf2298df88b6362df687205fd619525aef5c9
sundhar1947 <126653757+sundhar1947@users.noreply.github.com>
2023-03-04 10:45:06 +0530

    Add files via upload

---
+++ MAX OF 3.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=int(input("enter c value"))
+if(a>b)and(a>c):
+    print("a is bigger")
+elif(b>a)and(b>c):
+    print("b is bigger")
+else:
+    print("c is bigger")
