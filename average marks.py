9af2b091b2f084666e8747524408d665d447a97f
sundhar1947 <126653757+sundhar1947@users.noreply.github.com>
2023-03-04 09:41:31 +0530

    Add files via upload

---
+++ average marks.py
@@ -0,0 +1,5 @@
+maths=int(input("enter maths marks:"))
+physics=int(input("enter physics marks:"))
+chemistry=int(input("enter chemistry marks:"))
+average=(maths+physics+chemistry)/3
+print(average)
